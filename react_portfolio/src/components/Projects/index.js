8567c95a46e71624cf071d2c265e0451d1168955
shankarmadimi <madimishankar5@gmail.com>
2025-10-18 01:02:33 +0530

    updated projects componenent index.js

--- react_portfolio/src/components/Projects/index.js
+++ react_portfolio/src/components/Projects/index.js
@@ -1,6 +1,6 @@
 import './index.scss';
 import Loader from 'react-loaders';
-import portfolioImage from '../../assets/images/portfolioImage.png'; // Using your image name
+
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimateLetters';
 
@@ -22,20 +22,39 @@ const Projects = () => {
                 </h1>
                 <div className='projects'>
                     <div className='project'>
-                        <img src={portfolioImage} alt="React Portfolio" className='projectImg' />
+                        <img src={process.env.PUBLIC_URL + '/Ds Visualizer.png'} alt="Data Structure Visualizer" className='projectImg' />
                         <div className='projectContent'>
-                            <h3>React Portfolio</h3>
-                            <p>A modern portfolio website built with React, SCSS, and React Router, showcasing my skills, projects, and experience.</p>
+                            <h3>Data Structure Visualizer</h3>
+                            <p>An interactive web application for learning data structures through visual animations and step-by-step operations.</p>
                             <div className='skills'>
                                 <h4>Skills</h4>
                                 <ul>
-                                    <li>React</li>
-                                    <li>SCSS</li>
-                                    <li>React Router</li>
-                                    <li>Responsive Design</li>
+                                    <li>Next.js 14</li>
+                                    <li>TypeScript</li>
+                                    <li>Shadcn/ui</li>
+                                    <li>React Flow</li>
+                                    <li>Framer Motion</li>
                                 </ul>
                             </div>
-                            <a href="https://github.com/shankarmadimi/my_portfolio" target="_blank" rel="noreferrer">Source Code</a>
+                            <a href="https://github.com/shankarmadimi/Ds-visualizer?tab=readme-ov-file" target="_blank" rel="noreferrer">Source Code</a>
+                        </div>
+                    </div>
+                    <div className='project'>
+                        <img src={process.env.PUBLIC_URL + '/cricket face-off predictor.png'} alt="Cricket Face-Off Predictor" className='projectImg' />
+                        <div className='projectContent'>
+                            <h3>Cricket Face-Off Predictor</h3>
+                            <p>A web application to predict cricket match outcomes and provide insights using machine learning.</p>
+                            <div className='skills'>
+                                <h4>Skills</h4>
+                                <ul>
+                                    <li>Python</li>
+                                    <li>Flask</li>
+                                    <li>React.js</li>
+                                    <li>Node.js</li>
+                                    <li>Scikit-learn</li>
+                                </ul>
+                            </div>
+                            <a href="https://github.com/shankarmadimi/cricket-predictor" target="_blank" rel="noreferrer">Source Code</a>
                         </div>
                     </div>
                 </div>
